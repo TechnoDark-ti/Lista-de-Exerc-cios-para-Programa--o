ff591b81e9992fa5a23b64a27b9ceba7db85fa9a
Márcio Moda <69868170+DarkCells@users.noreply.github.com>
2024-01-20 14:04:38 -0300

    Revert "Atualizando o código"
    This reverts commit 6cf6dd729ddfda87c450470e988544295996ef24.

--- Java/Exercicio_Java/src/main/java/com/darkcells/QuestoesIFPA/Q2_IF.java
+++ Java/Exercicio_Java/src/main/java/com/darkcells/QuestoesIFPA/Q2_IF.java
@@ -4,17 +4,8 @@
  */
 package com.darkcells.QuestoesIFPA;
 
-import java.util.Scanner;
-
 public class Q2_IF {
     public void Questao2(){
-        Scanner scan = new Scanner(System.in);
-        
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        
-        System.out.println(a );
-        System.out.println(b);
         
     }
 }
--- Java/Exercicio_Java/src/main/java/main.java
+++ Java/Exercicio_Java/src/main/java/main.java
@@ -9,9 +9,7 @@ import com.darkcells.QuestoesIFPA.*;
 
 public class main {
     public static void main(String[] args) {
-        Q2_IF instancia = new Q2_IF();
         
-        instancia.Questao2();
         
     }
 }
